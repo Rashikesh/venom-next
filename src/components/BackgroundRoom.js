36e51e420f84ff8a2664bf4bfa756f29645738b1
GitHarshikesh <githarshikesh@gmail.com>
2026-06-04 12:55:37 +0530

    Revert "updated readme.md"
    This reverts commit 15db49e236d6f4924ddac83e0d9c0991fe35684a.
    git add src/components/BackgroundRoom.js
    git commit -m "Save temporary local changes"

--- src/components/BackgroundRoom.js
+++ src/components/BackgroundRoom.js
@@ -1,6 +1,7 @@
 export default function BackgroundRoom() {
   return (
     <div className="room">
+
       <div className="wall back-wall"></div>
 
       <div className="wall left-wall"></div>
@@ -9,21 +10,14 @@ export default function BackgroundRoom() {
 
       <div className="floor"></div>
 
-      <div className="floating-card one">
-        {/* <div class="main-preview">
-        <img
-          id="featured"
-          src="../images/main_preview.png"
-          alt="Featured Preview"
-        ></img>
-      </div> */}
-      </div>
+      <div className="floating-card one"></div>
 
       <div className="floating-card two"></div>
 
       <div className="floating-card three"></div>
 
       <div className="ambient-light"></div>
+
     </div>
   );
 }
